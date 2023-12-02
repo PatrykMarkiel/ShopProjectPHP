20f494953e5699f834be0a73a97f7339062941dc
Bułas <103049804+PatrykMarkiel@users.noreply.github.com>
2023-12-02 18:22:28 +0100

    the cart works

--- Cart.php
+++ Cart.php
@@ -7,27 +7,47 @@ if (isset($_COOKIE['cart']) && is_array(json_decode($_COOKIE['cart'], true))) {
     $cartItems = [];
 }
 
-if (isset($_GET['add_to_cart'])) {
+if (isset($_GET['add_to_cart']) && isset($_GET['quantity'])) {
     $productID = $_GET['add_to_cart'];
+    $quantity = $_GET['quantity'];
+
+    // Sprawdzenie czy ilość jest liczbą dodatnią
+    if (!is_numeric($quantity) || $quantity <= 0) {
+        echo "Invalid quantity.";
+        exit;
+    }
 
     $productFound = false;
 
     foreach ($cartItems as &$item) {
         if (isset($item['ID']) && $item['ID'] === $productID) {
-            $item['Availability']++;
+            $item['Availability'] += $quantity;
             $productFound = true;
             break;
         }
     }
 
     if (!$productFound) {
-        $cartItems[] = array('ID' => $productID, 'Availability' => 1);
+        $cartItems[] = array('ID' => $productID, 'Availability' => $quantity);
     }
 
     setcookie('cart', json_encode($cartItems), time() + 3600, "/");
 }
 
+if (isset($_GET['remove_from_cart'])) {
+    $productID = $_GET['remove_from_cart'];
 
+    foreach ($cartItems as $key => $item) {
+        if ($item['ID'] === $productID) {
+            unset($cartItems[$key]);
+            break;
+        }
+    }
+
+    $cartItems = array_values($cartItems);
+
+    setcookie('cart', json_encode($cartItems), time() + 3600, "/");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +55,84 @@ if (isset($_GET['add_to_cart'])) {
     <meta charset="UTF-8">
     <title>Cart</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="Icons/icon.png" alt="Link 2" class="d-inline-block align-text-top me-2" ID="brand" style="max-height: 25px;">
+      <b>Warland</b>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" ID="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="#"></a></li>
+        <li class="nav-item"><a class="nav-link" href="Main.php"><b>Main page</b></a></li>
+        <li class="nav-item"><a class="nav-link" href="Cart.php"><b>cart</b></a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><b>profile</b></a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><b>settings</b></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<!-- Content -->
+<div class="container my-5 rounded bg-secondary" style="padding: 20px;">
+    <h1>Cart</h1>
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Unit Price</th>
+                        <th>Total Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (!empty($cartItems)) {
+                        $totalPrice = 0;
+                        // Pętla wyświetlająca produkty w koszyku
+                        foreach ($cartItems as $item) {
+                            // Pobranie szczegółów produktu z bazy danych na podstawie $item['ID']
+                            $stmt = $mysqli->prepare("SELECT Name, Price FROM Products WHERE ID = ?");
+                            $stmt->bind_param("i", $item['ID']);
+                            $stmt->execute();
+                            $result = $stmt->get_result();
+                            $productDetails = $result->fetch_assoc();
+
+                            echo '<tr>';
+                            echo '<td>' . $productDetails['Name'] . '</td>';
+                            echo '<td>' . (isset($item['Availability']) ? $item['Availability'] : 0) . '</td>';
+                            echo '<td>$' . number_format($productDetails['Price'], 2) . '</td>';
+                            $totalItemPrice = (isset($item['Availability']) ? $productDetails['Price'] * $item['Availability'] : 0);
+                            echo '<td>$' . number_format($totalItemPrice, 2) . '</td>';
+                            echo '</tr>';
+
+                            $totalPrice += $totalItemPrice;
+                        }
+                        echo '
+                        <tr>
+                            <td><b>Total Price:</b></td>
+                            <td></td>
+                            <td></td>
+                            <td>$' . number_format($totalPrice, 2) . '</td>
+                        </tr>';
+                    } else {
+                        echo '<tr><td colspan="4">No items in the cart</td></tr>';
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<style>
     .card-img-top {
       height: 300px; 
       object-fit: cover;
@@ -71,74 +168,5 @@ if (isset($_GET['add_to_cart'])) {
             height: 100%;
         }
   </style>
-</head>
-<body>
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="Icons/icon.png" alt="Link 2" class="d-inline-block align-text-top me-2" ID="brand" style="max-height: 25px;">
-      <b>Warland</b>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" ID="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="#"></a></li>
-        <li class="nav-item"><a class="nav-link" href="Main.php"><b>Main page</b></a></li>
-        <li class="nav-item"><a class="nav-link" href="Cart.php"><b>cart</b></a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><b>profile</b></a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><b>settings</b></a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<br>
-<!-- Content -->
-<div class="container my-5 rounded bg-secondary" style="padding: 20px;">
-    <h1>Cart</h1>
-    <div class="row">
-        <div class="col">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    if (isset($_COOKIE['userData'])) {
-                        $userData = json_decode($_COOKIE['userData'], true);
-
-                        $totalPrice = 0;
-
-
-                        foreach ($cartItems as $item) {
-                            echo '<tr>';
-                            echo '<td>' . $item['ID'] . '</td>'; 
-                            echo '<td>' . $item['Availability'] . '</td>'; 
-
-                        }
-
-
-                        echo '
-                        <tr>
-                            <td><b>Total Price:</b></td>
-                            <td>$' . $totalPrice . '</td>
-                        </tr>';
-                    } else {
-                        echo 'No user data found.';
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- Product.php
+++ Product.php
@@ -1,96 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Product Details</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="Icons/icon.png" alt="Link 2" class="d-inline-block align-text-top me-2" ID="brand" style="max-height: 25px;">
-      <b>Warland</b>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" ID="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="#"></a></li>
-        <li class="nav-item"><a class="nav-link" href="Main.php"><b>Main page</b></a></li>
-        <li class="nav-item"><a class="nav-link" href="Cart.php"><b>Cart</b></a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><b>Profile</b></a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><b>Settings</b></a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<br><br>
-
-<!-- Content -->
-
-
-<?php
-include "Conn.php";
-
-if (isset($_GET['ID'])) {
-    $productID = $_GET['ID'];
-
-    // Pobierz informacje o produkcie na podstawie ID
-    $sql = "SELECT * FROM Products WHERE ID = $productID";
-    $result = $mysqli->query($sql);
-
-    if ($result->num_rows > 0) {
-        $productDetails = $result->fetch_assoc();
-?>
-<div class="container my-5 rounded bg-secondary" style=" padding: 20px;">
-    <div class="row align-items-center">
-        <h1 class="mb-4"><b><?php echo $productDetails['Name']; ?></b></h1>
-        <div class="col-md-6">
-            <div class="position-relative">
-                <img src="Photos/<?php echo $productDetails['Product_Link']; ?>" class="product-img img-fluid rounded" alt="<?php echo $productDetails['Name']; ?>">
-            </div>
-        </div>
-        <div class="col-md-6 ">
-            <div class="product-description border bg-white p-2 rounded" style="border-width: 5px; border-radius: 0;">
-                <h3 class="fw-bold"><?php echo $productDetails['Name']; ?></h3>
-                <span class="lead mb-4">Description: <?php echo $productDetails['Description']; ?></span>
-                <br>
-                <span class="lead mb-4 ">Price: $<?php echo intval($productDetails['Price']); ?></span>
-                <br>
-                <span class="lead mb-4 ">Year of production:<?php echo intval($productDetails['Production_year']); ?></span>
-                <br>
-                <span class="lead mb-4 ">Condition:<?php echo $productDetails['Condition']; ?></span>
-                <br>
-                <span class="lead mb-4 ">Company:<?php echo $productDetails['Company']; ?></span>
-
-                <div class="mb-4">
-                    <span class="lead mb-4 ">Country: <?php echo $productDetails['Country']?>
-                    <?php
-                    $countryFlagPath = 'Flags/' . $productDetails['Country'] . '.jpg';
-                    if (file_exists($countryFlagPath)) {
-                        echo '<img class="img-fluid" src="' . $countryFlagPath . '" alt="' . $productDetails['Country'] . '" height="150" width="50">';
-                    }
-                    ?>
-                </span>
-                <div class="mb-4">
-                    <form action="Cart.php" method="GET">
-                        <input type="hidden" name="add_to_cart" value="<?php echo $productDetails['ID']; ?>">
-                        <input type="number" name="Availability" class="form-control" min="1" max="<?php echo $productDetails['Availability']; ?>" value="1" required style="width: 80px;">
-                        <br>
-                        <button type="submit" class="btn btn-primary">Add to Cart</button>
-                    </form>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</div>
-
-<style>
+    <style>
         body {
             margin: 0;
             padding: 0;
@@ -109,20 +23,137 @@ if (isset($_GET['ID'])) {
             height: 100%;
         }
     </style>
+</head>
+<body class="content">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="Icons/icon.png" alt="Link 2" class="d-inline-block align-text-top me-2" ID="brand" style="max-height: 25px;">
+                <b>Warland</b>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" ID="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="#"></a></li>
+                    <li class="nav-item"><a class="nav-link" href="Main.php"><b>Main page</b></a></li>
+                    <li class="nav-item"><a class="nav-link" href="Cart.php"><b>Cart</b></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><b>Profile</b></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><b>Settings</b></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <br><br>
+
+    <!-- Content -->
 
     <?php
-        } else {
-            echo "Product not found.";
-        }
+    include "Conn.php";
 
-        $mysqli->close();
-    } else {
-        echo 'Error: No product identifier provided.';
-        exit;
-    }
+    if (isset($_GET['ID']) && is_numeric($_GET['ID'])) {
+        $productID = $_GET['ID'];
+
+        // Pobierz informacje o produkcie na podstawie ID (używając prepared statements, aby zabezpieczyć zapytanie)
+        $stmt = $mysqli->prepare("SELECT * FROM Products WHERE ID = ?");
+        $stmt->bind_param("i", $productID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $productDetails = $result->fetch_assoc();
     ?>
+
+    <div class="container my-5 rounded bg-secondary" style="padding: 20px;">
+        <div class="row align-items-center">
+            <h1 class="mb-4"><b><?php echo htmlspecialchars($productDetails['Name']); ?></b></h1>
+            <div class="col-md-6">
+                <div class="position-relative">
+                    <img src="Photos/<?php echo htmlspecialchars($productDetails['Product_Link']); ?>" class="product-img img-fluid rounded" alt="<?php echo htmlspecialchars($productDetails['Name']); ?>">
+                </div>
+            </div>
+            <div class="col-md-6 ">
+                <div class="product-description border bg-white p-2 rounded" style="border-width: 5px; border-radius: 0;">
+                    <h3 class="fw-bold"><?php echo htmlspecialchars($productDetails['Name']); ?></h3>
+                    <span class="lead mb-4">Description: <?php echo htmlspecialchars($productDetails['Description']); ?></span>
+                    <br>
+                    <span class="lead mb-4">Price: $<?php echo number_format($productDetails['Price'], 2); ?></span>
+                    <br>
+                    <span class="lead mb-4">Year of production: <?php echo intval($productDetails['Production_year']); ?></span>
+                    <br>
+                    <span class="lead mb-4">Condition: <?php echo htmlspecialchars($productDetails['Condition']); ?></span>
+                    <br>
+                    <span class="lead mb-4">Company: <?php echo htmlspecialchars($productDetails['Company']); ?></span>
+
+                    <div class="mb-4">
+                        <span class="lead mb-4">Country: <?php echo htmlspecialchars($productDetails['Country']); ?>
+                        <?php
+                        $countryFlagPath = 'Flags/' . htmlspecialchars($productDetails['Country']) . '.jpg';
+                        if (file_exists($countryFlagPath)) {
+                            echo '<img class="img-fluid" src="' . $countryFlagPath . '" alt="' . htmlspecialchars($productDetails['Country']) . '" height="150" width="50">';
+                        }
+                        ?>
+                        </span>
+                    </div>
+
+                    <div class="mb-4">
+                    <div class="mb-4">
+    <form action="Cart.php" method="GET">
+        <input type="hidden" name="add_to_cart" value="<?php echo htmlspecialchars($productDetails['ID']); ?>">
+        <input type="number" name="quantity" class="form-control" min="1" max="<?php echo htmlspecialchars($productDetails['Availability']); ?>" value="1" required style="width: 80px;">
+        <br>
+        <button type="submit" class="btn btn-primary">Add to Cart</button>
+    </form>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <?php
+    // Set cookies to store product information when adding to cart
+    if (isset($_GET['add_to_cart']) && isset($_GET['quantity'])) {
+        $productID = $_GET['add_to_cart'];
+        $quantity = $_GET['quantity'];
+
+        $cartItems = [];
+
+        if (isset($_COOKIE['cart']) && is_array(json_decode($_COOKIE['cart'], true))) {
+            $cartItems = json_decode($_COOKIE['cart'], true);
+        }
+
+        $productFound = false;
+
+        foreach ($cartItems as &$item) {
+            if (isset($item['ID']) && $item['ID'] === $productID) {
+                $item['Quantity'] += $quantity;
+                $productFound = true;
+                break;
+            }
+        }
+
+        if (!$productFound) {
+            $cartItems[] = array('ID' => $productID, 'Quantity' => $quantity);
+        }
+
+        setcookie('cart', json_encode($cartItems), time() + 3600, "/");
+    }
+    ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+            } else {
+                echo "Product not found.";
+            }
+
+            $stmt->close();
+            $mysqli->close();
+        } else {
+            echo 'Error: Invalid product identifier.';
+            exit;
+        }
+    ?>
 </body>
 </html>
